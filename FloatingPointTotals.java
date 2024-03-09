065225aef351cfe2e952491a78bc725666170ed9
Brian Gunther <brian.gunther@csuglobal.edu>
2024-03-09 11:29:33 -0800

    final commit

--- FloatingPointTotals.java
+++ FloatingPointTotals.java
@@ -1,3 +1,9 @@
+/* 
+ * CSC320 - CTA Module 4 - March 10, 2024
+ * Purpose: Use a while loop to gather five floating point values from a user, ensuring the loop terminates successfully. 
+ * Then, print the total, average, maximum, minimum, and interest on the total at 20%.
+ */
+
 package cta;
 import java.util.Scanner;
 
@@ -8,25 +14,41 @@ public class FloatingPointTotals {
       double userInput;
       double runningTotal = 0;
       
-      // Initialize reverse min/max values so that these variables can be used in conditional statements
-      double inputMin = Double.MAX_VALUE, inputMax = Double.MIN_VALUE;
+      // Initialize min/max values so that these variables can be used in conditional statements
+      double inputMin = 0, inputMax = 0;
       
-      // Input counter is used as the loop control variable
+      // Input counter is initialized at 0 to be used as the loop control variable
       int inputCounter = 0;
       
-      // Max loop constant is the loop entry/exit check
+      // Max loop constant is the loop entry/exit check and serves as a quantity value for computing average
       final int MAX_LOOP = 5;
       
       while (inputCounter < MAX_LOOP) {
          System.out.print("Please enter a decimal value: ");
+         
+         /* 
+          * Using the nextDouble() method performs rudimentary input validation to ensure improper values are not accepted.
+          * However, since there is no further exception handling, the program will terminate on a failed validation check.
+          */
          userInput = s.nextDouble();
          runningTotal += userInput;
          
-         if (userInput < inputMin) {
-            inputMin = userInput;
+
+         /* 
+          * On the first loop, the min/max variables must both be set to userInput to establish baseline.
+          * In subsequent loops, conditional checks are used to determine min/max values.
+          */
+         if (inputCounter > 0) {
+            if (userInput < inputMin) {
+               inputMin = userInput;
+            }
+            
+            if (userInput > inputMax) {
+               inputMax = userInput;
+            }
          }
-         
-         if (userInput > inputMax) {
+         else {
+            inputMin = userInput;
             inputMax = userInput;
          }
          
