f92564f821d4742793abb4e82a17cd3e7e08a2b4
Brian Gunther <brian.gunther@csuglobal.edu>
2024-03-08 21:56:15 -0800

    java code 1

--- FloatingPointTotals.java
+++ FloatingPointTotals.java
@@ -1,9 +1,20 @@
 package cta;
+import java.util.Scanner;
 
 public class FloatingPointTotals {
 
    public static void main(String[] args) {
-
+      Scanner s = new Scanner(System.in);
+      double userInput;
+      double inputMin, inputMax;
+      double runningTotal = 0;
+      
+      // Input counter is used as the loop control variable
+      int inputCounter = 0;
+      
+      // Max loop constant is the loop entry/exit check
+      final int MAX_LOOP = 5;
+      
    }
 
 }
